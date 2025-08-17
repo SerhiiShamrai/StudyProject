1bcbb9217b70debd49028e0776a2c92ac2bc458a
SerhiiShamrai <serhii_sh92@hotmail.com>
2025-08-17 13:51:27 +0300

    new version

--- script.js
+++ script.js
@@ -1,96 +1,99 @@
 "use strict";
 
-let numberOfFilms;
-
-start();
-
 let personalMovieDB = {
-    count: numberOfFilms,
+    count: undefined,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-}
+    privat: false,
 
-rememberMyFilms();
-
-detectPersonalLevel();
-
-showMyDB(personalMovieDB.private);
-
-writeYourGenres();
-
-function writeYourGenres()
-{
-    for (let i = 0; i < 3; i++)
+    writeYourGenres: function ()
     {
-        let favoriteGenre = prompt(`Ваш улюблений жанр під номером ${i + 1}?`);
-        if (!favoriteGenre)
+        for (let i = 0; i < 1; i++)
         {
-            i--;
+            let favoriteGenre = prompt(`Введіть Ваші улюблені жанри фільмів через кому`).split(', ');
+            if (!favoriteGenre)
+            {
+                console.log('Ви ввели некоректні дані!');
+                i--;
+            }
+            else
+            {
+                favoriteGenre.sort();
+                personalMovieDB.genres = favoriteGenre;
+            }
+        }
+
+
+        personalMovieDB.genres.forEach( function(value, count)
+        {
+           console.log(`Улюблений жанр №${count + 1} - це ${value}`);
+        });
+    },
+
+    showMyDB: function (hidden)
+    {
+        if (!hidden)
+        {
+            console.log(hidden);
+        }
+    },
+
+    detectPersonalLevel: function ()
+    {
+        if (personalMovieDB.count <= 10)
+        {
+            alert('Переглянуто доволі мало фільмів');
+        }
+        else if (personalMovieDB.count >= 30)
+        {
+            alert('Ви кіноман');
+        }
+        else if (isNaN(personalMovieDB.count))
+        {
+            alert('Виникла помилка');
         }
         else
         {
-            personalMovieDB.genres[i] = favoriteGenre; 
-        } 
-    }
-}
-
-function showMyDB(hidden)
-{
-    if (!hidden)
-    {
-        console.log(personalMovieDB);
-    }
-}
-
-
-function detectPersonalLevel() 
-{
-    if (personalMovieDB.count <= 10) 
-    {
-        alert('Переглянуто доволі мало фільмів');
-    }
-    else if (personalMovieDB.count >= 30) 
-    {
-        alert('Ви кіноман');
-    }
-    else if (isNaN(personalMovieDB.count)) 
-    {
-        alert('Виникла помилка');
-    }
-    else 
-    {
-        alert('Ви класичний глядач');
-    }
-}
-
-function rememberMyFilms() 
-{
-    for (let i = 0; i < 2; i++) 
-    {
-        let lastMovie = prompt('Один з останніх переглянутих фільмів?', ''), 
-            rating = prompt(`Як оціните фільм?`, '');
-        if (!lastMovie || lastMovie.length > 50 || !rating || rating.length > 50) 
-        {
-            console.log('error!');
-            i--;
+            alert('Ви класичний глядач');
         }
-        else 
-        {
-            personalMovieDB.movies[lastMovie] = rating;
-            console.log('done!');
-        }
-    }
-}
+    },
 
-
-function start() 
-{
-    do 
+    rememberMyFilms: function ()
     {
-        numberOfFilms = +prompt('Скільки фільмів ви вже переглянули?', '');
-    }   while (!numberOfFilms || isNaN(numberOfFilms));
-}
+        for (let i = 0; i < 2; i++)
+        {
+            let lastMovie = prompt('Один з останніх переглянутих фільмів?', '').trim(),
+                rating = prompt(`Як оціните фільм?`, '');
+            if (!lastMovie || lastMovie.length > 50 || !rating || rating.length > 50)
+            {
+                console.log('error!');
+                i--;
+            }
+            else
+            {
+                personalMovieDB.movies[lastMovie] = rating;
+                console.log('done!');
+            }
+        }
+    },
+
+    start: function ()
+    {
+        do
+        {
+            personalMovieDB.count = +prompt('Скільки фільмів ви вже переглянули?', '');
+        }   while (!personalMovieDB.count || isNaN(personalMovieDB.count));
+    },
+
+    toggleVisibleMyDB: function ()
+    {
+        personalMovieDB.privat = !personalMovieDB.privat;
+        console.log(personalMovieDB.privat);
+    }
+};
+
+
+
 
 
